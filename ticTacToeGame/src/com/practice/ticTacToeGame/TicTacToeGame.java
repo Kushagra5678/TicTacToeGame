fb8cb3ae2cf252a6fbec5bd352f375cf533ee545
Kushagra5678 <kushu.7feb@gmail.com>
2020-10-05 11:40:50 +0530

    UC4 commit

--- ticTacToeGame/src/com/practice/ticTacToeGame/TicTacToeGame.java
+++ ticTacToeGame/src/com/practice/ticTacToeGame/TicTacToeGame.java
@@ -1,39 +1,63 @@
 package com.practice.ticTacToeGame;
+
 import java.util.*;
+
 public class TicTacToeGame {
-	static Scanner sc;
+	private static Scanner sc = new Scanner(System.in);
 	private static char[] board;
+	private static char player;
+	private static char computer;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic-Tac-Toe Game");
-		board=new char[10];
+		board = new char[10];
 		initializeBoard();
-		char player1=chooseInput();
-		char computer=(player1=='X')? 'O': 'X';
-		printBoard(board);
+		player = chooseInput();
+		computer = (player == 'X') ? 'O' : 'X';
+		printBoard();
+		System.out.println("Player will start the game");
+		char turn = player;
+		turn = startGame(turn);
+		printBoard();
 		sc.close();
 	}
-	// To get a game view UC3
-	private static void printBoard(char[] board) {
+	//UC4 first move in the game
+	private static char startGame(char turn) {
+		//sc=new Scanner(System.in);
+		System.out.println("Enter position between 1 to 9: ");
+		int position = sc.nextInt();
+		if(board[position] == ' ') {
+			board[position] = turn;
+			turn= (turn == player) ? computer : player;
+		}
+		else
+			System.out.println("Invalid move Enter again");
+		return turn;
+	}
+
+	// UC3 To get a board view
+	private static void printBoard() {
 		System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
 		System.out.println("--------------");
 		System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
 		System.out.println("--------------");
 		System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
 	}
-	//UC2
+
+	// UC2 input from user
 	private static char chooseInput() {
-		sc=new Scanner(System.in);
+		//sc = new Scanner(System.in);
 		System.out.println("Choose X or O:");
-		char player1=sc.next().charAt(0);
-		if(player1=='X')
-			player1='X';
-		else if(player1=='O')
-			player1='O';
-		return player1;
+		char player = sc.next().charAt(0);
+		if (player == 'X')
+			player = 'X';
+		else if (player == 'O')
+			player = 'O';
+		return player;
 	}
-	//UC1
+
+	// UC1 Initialize board with empty spaces
 	private static void initializeBoard() {
 		Arrays.fill(board, ' ');
 	}
 }
-
