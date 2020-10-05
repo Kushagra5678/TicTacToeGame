903370a3f6124b665dc0207457dc282b44a5c6c2
Kushagra5678 <kushu.7feb@gmail.com>
2020-10-05 09:59:59 +0530

    UC2 commit

--- ticTacToeGame/src/com/practice/ticTacToeGame/TicTacToeGame.java
+++ ticTacToeGame/src/com/practice/ticTacToeGame/TicTacToeGame.java
@@ -1,15 +1,31 @@
 package com.practice.ticTacToeGame;
-
+import java.util.*;
 public class TicTacToeGame {
+	static Scanner sc;
+	private static String[] board;
 	public static void main(String[] args) {
-		String[] board=new String[10];
+		System.out.println("Welcome to Tic-Tac-Toe Game");
+		board=new String[10];
 		printBoard(board);
+		String turn=chooseInput();
+		sc.close();
 	}
-	static void printBoard(String board[]) {
+	// To get a game view
+	private static void printBoard(String board[]) {
 		System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
 		System.out.println("----------------------");
 		System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
 		System.out.println("----------------------");
 		System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
 	}
+	private static String chooseInput() {
+		System.out.println("Choose X or O:");
+		String player1=sc.nextLine();
+		if(player1=="X")
+			player1="X";
+		else if(player1=="O")
+			player1="O";
+		return player1;
+	}
 }
+
